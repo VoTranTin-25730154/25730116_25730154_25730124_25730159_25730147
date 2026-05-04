cd6dcb2dc026533acc64f7126091177ae33608ad
25730147 - Trần Nguyễn Lâm Thành <25730147@ms.uit.edu.vn>
2026-05-04 11:10:34 +0700

    Common: Declare global shared variables for score and board state

--- Common.h
+++ Common.h
@@ -8,3 +8,7 @@ const int KEY_RIGHT = 77;
 const int KEY_DOWN = 80;
 const int KEY_UP = 72;
 const int KEY_ESC = 27;
+
+extern int board[BOARD_HEIGHT][BOARD_WIDTH];
+extern int score;
+extern int highScore;
