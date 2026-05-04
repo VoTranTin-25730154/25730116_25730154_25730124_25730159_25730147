c533f593d2ed6858813be347f754e89bb0773ab2
25730147 - Trần Nguyễn Lâm Thành <25730147@ms.uit.edu.vn>
2026-05-04 11:14:23 +0700

    Render: Implement gotoxy function using Windows API

--- Render.cpp
+++ Render.cpp
@@ -7,3 +7,10 @@ using namespace std;
 
 const int OFFSET_X = 2;
 const int OFFSET_Y = 1;
+
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
