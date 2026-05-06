bf83ed946b1b50053ac3c189444e1c6369686bb8
UIT 25730159 <25730159@ms.uit.edu.vn>
2026-05-06 19:57:53 +0700

    Update Common.h
    Define input key mappings

--- Common.h
+++ Common.h
@@ -1,5 +1,12 @@
 #pragma once
+
 #include <conio.h>
 
 const int BOARD_WIDTH = 10;
 const int BOARD_HEIGHT = 20;
+
+const int KEY_LEFT = 75;
+const int KEY_RIGHT = 77;
+const int KEY_DOWN = 80;
+const int KEY_UP = 72;
+const int KEY_ESC = 27;
