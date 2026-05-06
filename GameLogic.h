d26b0c5680ceb09855c65774c716804ac2b64636
25730116-Vu-Quang-Huy <25730116@ms.uit.edu.vn>
2026-05-06 18:45:40 +0700

    Update GameLogic.h
    Logic: Add high score management prototypes to GameLogic.h

--- GameLogic.h
+++ GameLogic.h
@@ -4,3 +4,5 @@ bool checkCollision(int type, int rotation, int x, int y);
 void lockPiece(int type, int rotation, int x, int y);
 void handleClearRows();
 void resetBoard();
+void loadHighScore();
+void saveHighScore();
