ae3b3d50a14c044e9569e2d1c45172fb5a121df7
UIT 25730159 <25730159@ms.uit.edu.vn>
2026-05-06 19:59:10 +0700

    Update Common.h
    Declare global shared variables for score and board state

--- Common.h
+++ Common.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <conio.h>
 
 const int BOARD_WIDTH = 10;
@@ -10,3 +9,7 @@ const int KEY_RIGHT = 77;
 const int KEY_DOWN = 80;
 const int KEY_UP = 72;
 const int KEY_ESC = 27;
+
+extern int board[BOARD_HEIGHT][BOARD_WIDTH];
+extern int score;
+extern int highScore;
