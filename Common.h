ad6f88a263522a18dded0c57567487cfcc95e0e7
25730124_UIT_NguyenThanhLong <thanhlong.vers304@gmail.com>
2026-05-10 21:03:18 +0700

    Update Common.h
    Common: Define input key mappings for gameplay

--- Common.h
+++ Common.h
@@ -3,3 +3,10 @@
 
 const int BOARD_WIDTH = 10;
 const int BOARD_HEIGHT = 20;
+const int BOARD_HEIGHT = 20;
+
+const int KEY_LEFT = 75;
+const int KEY_RIGHT = 77;
+const int KEY_DOWN = 80;
+const int KEY_UP = 72;
+const int KEY_ESC = 27;
