278325eb55070d0171f3a17dcb8d84e0374af23e
25730124_UIT_NguyenThanhLong <thanhlong.vers304@gmail.com>
2026-05-12 18:58:11 +0700

    Update GameLogic.h
    Main: Finalize main loop with Game Over state and Play Again functionality

--- GameLogic.h
+++ GameLogic.h
@@ -1,1 +1,8 @@
 #pragma once
+
+bool checkCollision(int type, int rotation, int x, int y);
+void loadHighScore();
+void lockPiece(int type, int rotation, int x, int y);
+void handleClearRows();
+void resetBoard();
+void saveHighScore();
