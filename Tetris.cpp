66e405a8092ffb65fa2db542312104000c61e5db
25730124_UIT_NguyenThanhLong <thanhlong.vers304@gmail.com>
2026-05-12 18:44:16 +0700

    Update Tetris.cpp
    Main: Setup initial headers and main game entry point

---
+++ Tetris.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include "Common.h"
+#include "Render.h"
+#include "Tetromino.h"
+#include "GameLogic.h"
+
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+
+    int speed = 25;
+
+    bool isKeepPlaying = true;
+    bool isSpecialKey = false;
+
+    return 0;
+}
