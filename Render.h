0d5865f8481a34d2617222ed668867cd7f70d34e
25730124_UIT_NguyenThanhLong <thanhlong.vers304@gmail.com>
2026-05-12 19:01:34 +0700

    Update Render.h
    Main: Finalize main loop with Game Over state and Play Again functionality

--- Render.h
+++ Render.h
@@ -7,3 +7,4 @@ void drawPiece(int type, int rotation, int x, int y);
 void clearPiece(int type, int rotation, int x, int y);
 void playGameSound(int type);
 void drawBoard();
+void gotoxy(int x, int y);
