42c3561be68aebe0f33ed5d54cda1f49b88522df
25730124_UIT_NguyenThanhLong <thanhlong.vers304@gmail.com>
2026-05-12 18:52:11 +0700

    Update Render.h
    Define required function prototypes in Render and GameLogic headers

--- Render.h
+++ Render.h
@@ -1,1 +1,9 @@
 #pragma once
+#pragma once
+
+void drawBoundary();
+void drawScore(int score);
+void drawPiece(int type, int rotation, int x, int y);
+void clearPiece(int type, int rotation, int x, int y);
+void playGameSound(int type);
+void drawBoard();
